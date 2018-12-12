c53bd414b3f3fb7035e3892256750541194bb3f6
agtourcj3 <lucian.maniu@gmail.com>
2018-12-12 21:12:02 +0200

    studied classes, variable and primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,7 +7,35 @@ package org.fasttrackit;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   //declaring a variable and instantiation a Car object
+        // this is also called initializing a variable
+       Car firstCompetitor = new Car();
+
+       firstCompetitor.name = "Dacia";
+       firstCompetitor.color = "black";
+       firstCompetitor.doorCount = 5;
+       firstCompetitor.mileage = 9.5;
+
+       //local variables are variables declared inside a method
+        //they don't receive default values
+
+        Car secondCompetitor = new Car();
+        secondCompetitor.name = "Golf";
+
+        Engine firstEngine = new Engine();
+        firstEngine.manufacturer = "Renault";
+        //prints a variable
+        System.out.println(firstEngine.manufacturer);
+
+        firstCompetitor.engine = firstEngine;
+
+        firstCompetitor.engine.manufacturer = "BMW";
+
+
+        System.out.println(firstEngine.manufacturer);
+        System.out.println(firstCompetitor.engine.manufacturer);
+
     }
+
+
 }
--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,13 +1,15 @@
 package org.fasttrackit;
 
 public class Car {
-
+    //instance variables declared as properties of a class
+    //they receive dafault values:null for objects, 0 for numbers and false for boolean
     String name;
     String color;
     int doorCount;
     double mileage; //consum//
-
     boolean running;
     long totalKm;
+    Engine engine; //compozitie, relatie de tip "has-a"
+
 
 }
