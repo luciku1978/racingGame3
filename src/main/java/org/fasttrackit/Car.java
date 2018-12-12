98e64007b9c5082164ea9a363ef45741f73f0c07
agtourcj3 <lucian.maniu@gmail.com>
2018-12-12 19:20:29 +0200

    creat clase

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    String color;
+    int doorCount;
+    double mileage; //consum//
+
+    boolean running;
+    long totalKm;
+
+}
